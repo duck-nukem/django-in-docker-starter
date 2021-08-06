8ce777d8c8da270d951c673314a383282946f5dc
Alex Szabó <kreatemore@gmail.com>
2021-08-07 00:19:55 +0200

    Updated uvicorn call to be compatible with distroless image

--- src/bin/start.sh
+++ src/bin/start.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate
 python manage.py collectstatic --no-input
-uvicorn app.asgi:application --host app --port 8000 --reload
+python -m uvicorn app.asgi:application --host app --port 8000 --reload
