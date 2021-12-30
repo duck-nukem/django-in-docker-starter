ca0244cbe818a7cf8fbb1870340340f55128c945
Alex Szabó <kreatemore@gmail.com>
2021-12-30 21:39:49 +0100

    Enables force restarting kubernetes pods with system downtime

--- infrastructure/bin/start_local.sh
+++ infrastructure/bin/start_local.sh
@@ -1,3 +1,5 @@
 eval $(minikube -p minikube docker-env)
-docker-compose build
+docker-compose build --parallel
+kubectl delete -f infrastructure/k8s/app-deployment.yaml || true
+kubectl delete -f infrastructure/k8s/web-deployment.yaml || true
 kubectl apply -f infrastructure/k8s
