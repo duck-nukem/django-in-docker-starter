f80ce3a053e8453f295a0a5ca1f9f933d9d342d1
Alex Szabó <kreatemore@gmail.com>
2021-12-31 10:13:42 +0100

    Removes unnecessary attribute from nc and reduces timeout for checking DB connectivity

--- src/bin/start.sh
+++ src/bin/start.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-until nc -zv $DB_SERVICE_HOST $DB_SERVICE_PORT
+until nc -z $DB_SERVICE_HOST $DB_SERVICE_PORT
 do
     echo "Waiting for DB Connection..."
-    sleep 3
+    sleep 1
 done
 
 python manage.py migrate
 python manage.py collectstatic --no-input
+# TODO: Separate dev & prod (use gunicorn)
 python -m uvicorn app.asgi:application --host 0.0.0.0 --port 8000 --reload
