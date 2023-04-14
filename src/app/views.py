888532effe7e88c54a2c6d39d6810360b4828827
Alex Szabó <kreatemore@gmail.com>
2023-04-14 12:23:22 +0200

    Adds DB response time to healthcheck response

--- src/app/views.py
+++ src/app/views.py
@@ -1,7 +1,21 @@
 import os
+import time
 
+from django.db import connection
 from django.http import JsonResponse, HttpRequest
 
 
 def healthcheck_view(_request: HttpRequest) -> JsonResponse:
-    return JsonResponse({'status': '❤️', 'hostname': os.getenv('HOSTNAME', 'unspecified')})
+    start_time = time.monotonic()
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT 1')
+        cursor.fetchone()
+    end_time = time.monotonic()
+
+    db_response_time = end_time - start_time
+
+    return JsonResponse({
+        'status': 'OK',
+        'hostname': os.getenv('HOSTNAME', 'unspecified'),
+        'db_response_time': f'{db_response_time:.4f}s',
+    })
